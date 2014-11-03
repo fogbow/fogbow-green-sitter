0d1d1e7ffbfcd7ab9bfafa408d6fa34483a53b25
aninhacostaribeiro <aninhacostaribeiro@gmail.com>
2014-11-03 13:17:58 -0300

    updated the class test

---
+++ src/main/java/org/fogbowcloud/green/agent/testeMain.java
@@ -0,0 +1,10 @@
+package org.fogbowcloud.green.agent;
+
+
+import java.io.IOException;
+
+public class testeMain {
+public static void main(String[] args) throws RuntimeException, IOException {
+	
+}
+}
