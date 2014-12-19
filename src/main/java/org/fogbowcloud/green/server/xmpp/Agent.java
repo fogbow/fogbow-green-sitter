b39f36ebaadbca57ab9db07ebaff5e7bca2ff478
aninhacostaribeiro <aninhacostaribeiro@gmail.com>
2014-12-19 16:38:37 -0300

    Added the class Agent

---
+++ src/main/java/org/fogbowcloud/green/server/xmpp/Agent.java
@@ -0,0 +1,26 @@
+package org.fogbowcloud.green.server.xmpp;
+
+public class Agent {
+	private String hostName;
+	private String ip;
+	private String jid;
+	
+	public Agent(String hostName, String jid, String ip) {
+		this.hostName = hostName;
+		this.jid = jid;
+		this.ip = ip;
+	}
+
+	public String getHostName() {
+		return hostName;
+	}
+
+	public String getIp() {
+		return ip;
+	}
+
+	public String getJid() {
+		return jid;
+	}
+
+}
