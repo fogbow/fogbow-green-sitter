4b3d14a7e4ef1f9dea58dcaafe55a3ec7a7fc193
aninhacostaribeiro <aninhacostaribeiro@gmail.com>
2015-02-10 11:29:12 -0300

    Changed the name of the package "XMPP" for "Communication"

---
+++ src/main/java/org/fogbowcloud/green/server/communication/GreenSitterCommunicationComponent.java
@@ -0,0 +1,42 @@
+package org.fogbowcloud.green.server.communication;
+
+import java.util.LinkedList;
+import java.util.Properties;
+
+import org.fogbowcloud.green.server.communication.Agent;
+import org.fogbowcloud.green.server.core.greenStrategy.GreenStrategy;
+import org.jamppa.component.XMPPComponent;
+
+public class GreenSitterCommunicationComponent extends XMPPComponent {
+
+	private GreenStrategy gs;
+	private LinkedList<Agent> listAgent = new LinkedList<Agent>(); 
+
+	public GreenSitterCommunicationComponent(Properties prop, GreenStrategy gs) {
+		super(prop.getProperty("xmpp.jid"), prop.getProperty("xmpp.password"), 
+				prop.getProperty("xmpp.host"), Integer.parseInt(prop.getProperty("xmpp.port")));
+		this.gs = gs;
+		addHandlers();
+	}
+	
+	public void setAgentAddress(String hostName, String jid, String ip) {
+		Agent agent = new Agent(hostName, jid, ip);
+		listAgent.add(agent);
+	}
+	
+	public void sendHostToBed(String host){
+		
+	}
+	
+	public void wakeUpHost(String host){
+		
+	}
+
+	private void addHandlers() {
+		IAmAliveHandler alive = new IAmAliveHandler(gs);
+		WakeUpRequestHandler wakeup = new WakeUpRequestHandler(gs);
+		addGetHandler(alive);
+		addGetHandler(wakeup);
+	}
+
+}
---
+++ src/main/java/org/fogbowcloud/green/server/communication/IAmAliveHandler.java
@@ -0,0 +1,31 @@
+package org.fogbowcloud.green.server.communication;
+
+import org.fogbowcloud.green.server.core.greenStrategy.GreenStrategy;
+import org.jamppa.component.handler.AbstractQueryHandler;
+import org.xmpp.packet.IQ;
+
+public class IAmAliveHandler extends AbstractQueryHandler {
+	
+	private static final String NAMESPACE = "org.fogbowcloud.green.IAmAlive";
+	private final GreenStrategy gs;
+
+	public IAmAliveHandler(GreenStrategy gs) {
+		super(NAMESPACE);
+		this.gs = gs;
+	}
+
+	@Override
+	public IQ handle(IQ query) {
+		//expected content format: IP plus host name (eg. "123.456.789 bobo")
+		String ip = query.getElement().element("query").elementText("ip");
+		String hostName = query.getElement().element("query").elementText("hostName");
+		String jid = query.getFrom().toString();
+		
+		gs.setAgentAddress(hostName, jid, ip);
+
+		IQ resultIQ = IQ.createResultIQ(query);
+
+		return resultIQ;
+	}
+
+}
