e6282f296b6f9e6a3c9fbfc2bbf5ace9e5f09ab8
aninhacostaribeiro <aninhacostaribeiro@gmail.com>
2015-03-27 16:24:19 -0300

    Added DateWrapper

---
+++ src/main/java/org/fogbowcloud/green/server/core/greenStrategy/DateWrapper.java
@@ -0,0 +1,11 @@
+package org.fogbowcloud.green.server.core.greenStrategy;
+
+import java.util.Date;
+
+public class DateWrapper {
+
+	public long getTime() {
+		return new Date().getTime();
+	}
+	
+}
